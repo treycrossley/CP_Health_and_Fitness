624b5ff5f849332f48a3d0129db2d6a46b1447d3
Trey Crossley <treycrossley@gmail.com>
2015-10-10 16:30:54 +0000

    Initialize repository

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get bio" do
+    get :bio
+    assert_response :success
+  end
+
+end
